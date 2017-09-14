a13e59eb7802456fe8f378ba487a9a9b9148b944
Alonso <code@zhangwu.space>
2017-09-14 20:54:16 +0800

    Simplify code

--- ContactsCoreData/ViewController.swift
+++ ContactsCoreData/ViewController.swift
@@ -8,13 +8,10 @@
 
 import Cocoa
 
-class ViewController: NSViewController,NSFetchedResultsControllerDelegate {
+class ViewController: NSViewController {
 
     @IBOutlet var arrayController: NSArrayController!
-    
     @IBOutlet weak var tableView: NSTableView!
-    
-    var controller: NSFetchedResultsController<SW>!
     
     dynamic lazy var swManager: SWManager = {
         return SWManager()
@@ -23,77 +20,29 @@ class ViewController: NSViewController,NSFetchedResultsControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         try? self.arrayController.fetch(with: nil, merge: false)
-//        let fetchRequest: NSFetchRequest<SW> = SW.fetchRequest()
-//        let dateSort = NSSortDescriptor(key: "chinesename", ascending: false)
-//        fetchRequest.sortDescriptors = [dateSort]
-//        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.swManager.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
-//        controller.delegate = self
-//        self.controller = controller
-//        do {
-//            try controller.performFetch()
-//        } catch {
-//            let error = error as NSError
-//            print("\(error)")
-//        }
     }
     
     override func viewDidAppear() {
         self.swManager.persistentContainer.viewContext.undoManager = self.undoManager
     }
     
-    @IBAction func addClassesAction(_ sender: NSButton){
+    @IBAction func addAction(_ sender: NSButton){
         let swes = NSEntityDescription.insertNewObject(forEntityName: "SW", into: self.swManager.persistentContainer.viewContext) as! SW
         swes.chinesename = "new";
     }
     
-    @IBAction func deleteClassesAction(_ sender: NSButton){
+    @IBAction func deleteAction(_ sender: NSButton){
         let selectedObjects:[SW]  = self.arrayController.selectedObjects as! [SW]
         for classObject: SW in selectedObjects {
             self.swManager.persistentContainer.viewContext.delete(classObject)
         }
     }
     
-    @IBAction func uploadPhotoAction(_ sender: NSButton){
-        let index = self.tableView.selectedRow
-        if index < 0 {
-            return
-        }
-        //self.openSelectClassPhotoFilePanel()
-    }
-    
-    func openSelectClassPhotoFilePanel() {
-        let openDlg = NSOpenPanel()
-        openDlg.canChooseFiles = true
-        openDlg.canChooseDirectories = false
-        openDlg.allowsMultipleSelection = false
-        openDlg.allowedFileTypes = ["png"]
-        openDlg.begin(completionHandler: { [weak self]  result in
-            if(result == NSFileHandlingPanelOKButton){
-                let fileURLs = openDlg.urls
-                for url:URL in fileURLs  {
-                    let image = NSImage(contentsOf: url as URL)
-                    let imageRepresentations = image?.representations
-                    let imageData = NSBitmapImageRep.representationOfImageReps(in: imageRepresentations!, using: .PNG, properties: [:])
-                    let arrangedObjects = self?.arrayController.arrangedObjects as! [SW]
-                    let index = self?.tableView.selectedRow
-                    let classObj = arrangedObjects[index!]
-                    classObj.photo = imageData as NSData?
-                }
-            }
-        })
-    }
-    
     @IBAction func saveAction(_ sender: NSButton){
-        //let arrangedObjects = self.arrayController.arrangedObjects as! [SW]
-       // let count = controller.sections?.count ?? 0
-//        for index in 0..<count {
-//            let classObj = arrangedObjects[index]
-//            //print(classObj.description)
-//        }//
         self.swManager.saveAction(sender)
     }
     
-    @IBAction func queryClassesAction(_ sender: NSSearchField) {
+    @IBAction func queryAction(_ sender: NSSearchField) {
         let content = sender.stringValue
         if content.characters.count <= 0 {
             return
@@ -102,13 +51,11 @@ class ViewController: NSViewController,NSFetchedResultsControllerDelegate {
         self.arrayController.filterPredicate = predicate
     }
     
-    @IBAction func undo(_ sender: AnyObject){
+    @IBAction func undoAction(_ sender: NSButton) {
         self.swManager.persistentContainer.viewContext.undoManager?.undo()
     }
     
-    @IBAction func redo(_ sender: AnyObject){
+    @IBAction func redoAction(_ sender: NSButton) {
         self.swManager.persistentContainer.viewContext.undoManager?.redo()
     }
-
 }
-
